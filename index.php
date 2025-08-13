454956c8faacfabe64257a7ffb0526f8fa637e3b
Hatem Said <hatem.said50@gmail.com>
2025-08-13 14:50:36 +0300

    Update index.php
    Co-authored-by: ellipsis-dev[bot] <65095814+ellipsis-dev[bot]@users.noreply.github.com>

--- index.php
+++ index.php
@@ -5,5 +5,5 @@ $phpmailer->isSMTP();
 $phpmailer->Host = 'sandbox.smtp.mailtrap.io';
 $phpmailer->SMTPAuth = true;
 $phpmailer->Port = 2525;
-$phpmailer->Username = '23af5bfdab5717';
+$phpmailer->Username = getenv('MAIL_USERNAME');
 $phpmailer->Password = '****7eea';
